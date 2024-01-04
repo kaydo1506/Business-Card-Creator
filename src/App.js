5e519fb6287544f6acc7099b96e3096bc23679fd
Rachael Okedo <okedoifeoma@yahoo.com>
2024-01-05 00:14:03 +0100

    Add CardEditor component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CardEditor from './components/CardEditor';
 
 function App() {
   return (
     <div className='App'>
       <h1 className='text-3xl font-bold underline'>Business Card Creator</h1>
-      {/* Other components will be added here */}
+      <CardEditor/>
     </div>
   );
 }
---
+++ src/components/CardEditor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CardEditor = () => {
+  return (
+    <div className='card-editor'>
+      <h2 className='text-xl font-semibold'>Card Editor</h2>
+      {/* Toolbar and Canvas components will go here */}
+    </div>
+  );
+};
+
+export default CardEditor;
